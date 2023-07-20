4c74d0c6978a0bce423c557de73c66c75896c0b6
Lucas Carter <lbc21@duke.edu>
2023-07-19 23:07:37 -0400

    Template and layout for coding projects

--- src/components/CodePage.vue
+++ src/components/CodePage.vue
@@ -1,6 +1,54 @@
 <template>
-  <div>
-    <p class="centered-text">Code demos coming soon!</p>
+  <div class="outer-container">
+    <div class="project" id="froyo">
+      <div class="left">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+      <div class="middle">
+        <h1>FROYO GAMES</h1>
+        <p>This is where project descriptions will go. They'll include the stack used, as well as what the project does and the features if any.</p>
+      </div>
+      <div class="right">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+    </div>
+    <div class="project" id="amazon">
+      <div class="left">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+      <div class="middle">
+        <h1>MINI AMAZON</h1>
+        <p>This is where project descriptions will go. They'll include the stack used, as well as what the project does and the features if any.</p>
+      </div>
+      <div class="right">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+    </div>
+    <div class="project" id="spotify">
+      <div class="left">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+      <div class="middle">
+        <h1>SPOTIFY SORTER</h1>
+        <p>This is where project descriptions will go. They'll include the stack used, as well as what the project does and the features if any.</p>
+      </div>
+      <div class="right">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+    </div>
+    <div class="project" id="french">
+      <div class="left">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+      <div class="middle">
+        <h1>FRENCH FLASH CARD MAKER</h1>
+        <p>This is where project descriptions will go. They'll include the stack used, as well as what the project does and the features if any.</p>
+      </div>
+      <div class="right">
+        <img src="../assets/LucasHeadshotCircle.png" id="my-photo"/>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,8 +61,20 @@ export default {
 
 <style>
 
-.centered-text {
-  text-align: center;
+.project {
+  margin: auto;
+  display: flex;
+  padding: 2em
+}
+
+.left {
+
+}
+.middle {
+
+}
+.right {
+
 }
 
 </style>
